a0ee7e65d1f6b2eab89b8179eb1843a835b68ab2
tomdud9907 <tomdud9907@github.com>
2022-10-11 08:46:10 +0100

    get article by id finished

--- models/models.js
+++ models/models.js
@@ -7,6 +7,38 @@ function selectTopics () {
   });
 };
 
-module.exports = {selectTopics}
+function selectArticlesByID (article_id) {
+  return db.query(`SELECT * FROM articles WHERE article_id=$1;`, [article_id])
+  .then(({ rows }) => {
+    const article = rows[0];
+    if (!article) {
+      return Promise.reject({
+        status: 404,
+        msg: `No article found for article_id: ${article_id}`,
+      });
+    }
+    return article;
+  });
+};
+module.exports = {selectTopics, selectArticlesByID}
 
 //tylko eksport
+
+
+// function selectArticlesByID (article_id) {
+//   return db
+//     .query(
+//       "SELECT * FROM articles WHERE article_id = $1",
+//       [article_id]
+//     )
+//     .then(({ rows }) => {
+//       const article = rows[0];
+//       if (!article) {
+//         return Promise.reject({
+//           status: 404,
+//           msg: `No article found for article_id: ${article_id}`,
+//         });
+//       }
+//       return article;
+//     });
+// };
