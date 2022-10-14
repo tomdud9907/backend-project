050b2b7045b18c460804c7b3ff3a7f95626b1ff0
tomdud9907 <tomdud9907@github.com>
2022-10-14 09:12:15 +0100

    delete comment has been added

--- models/models.js
+++ models/models.js
@@ -124,7 +124,7 @@ function insertComment (body, username, article_id) {
 
   function removeCommentById (comment_id) {
    console.log(comment_id)
-    return db.query('DELETE FROM comments WHERE comment_id = $1', [comment_id])
+    return db.query('DELETE FROM comments WHERE comment_id = $1;', [comment_id])
     .then((response) => {
       if(response.rowCount === 0){
         return Promise.reject({status: 404, msg: 'comment doesnt exist'})
